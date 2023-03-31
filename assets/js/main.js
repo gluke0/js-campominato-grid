b6a1ef117d6cafdbbbc285bd91682c693bac9ad3
gluke0 <milanesigianluca@gmail.com>
2023-03-31 16:13:33 +0200

    grafica html già con livelli

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+// bottone genera una griglia quadrata
+// 100 caselle con num progressivo 10x10
+// utente clicca cellasi colora di azzurro e in console vedo num cella
+    // select con 3 difficoltà  
+    // E = 100 (10x10) - M = 81 (9x9) - H = 49 (7x7) 
