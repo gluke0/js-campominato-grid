39f57316e1030a8019a192a51b64139d9b852fb6
gluke0 <milanesigianluca@gmail.com>
2023-04-04 17:26:57 +0200

    ordine codice css

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,38 +4,36 @@
     // select con 3 difficoltà  
     // E = 100 (10x10) - M = 81 (9x9) - H = 49 (7x7) 
 
-let button = document.querySelector('#button');
-let difficulty = document.querySelector('select');
-let structure = document.querySelector('#section');
-
-// parte la partita al click del bottone
-button.addEventListener('click', function(){
-    game();
-})
-
-function game(){
-    let level = parseInt(difficulty.value);
-
-    structure.innerHTML = '';
-
-    // genero griglia
-    let grid = document.createElement('div');
-    grid.classList.add('grid');
-
-    structure.append(grid);
-
-    for(let i = 1; i <= level; i++){
-
-        let box = document.createElement('div');
-        box.classList.add('box');
-        box.innerText = i; 
-
-        document.querySelector('.grid').append(box);
-
-        box.addEventListener('click', function(){
-            this.classList.toggle('played');
-            console.log(this.innerText)
-
-        })
+    let button = document.querySelector('button');
+    let difficulty = document.querySelector('select');
+    let structure = document.querySelector('#section');
+    
+    // click per iniziare partita
+    button.addEventListener('click', function(){
+        game();
+    })
+    
+    function game(){
+        let level = parseInt(difficulty.value);
+    
+        
+    
+        let grid = document.createElement('div');
+        grid.classList.add('grid');
+    
+        structure.append(grid);
+    
+        for(let i = 1; i <= level; i++){
+    
+            let box = document.createElement('div');
+            box.classList.add('box');
+            box.innerText = i;
+    
+            document.querySelector('.grid').append(box);
+    
+            box.addEventListener('click', function(){
+                this.classList.toggle('played');
+                console.log(this.innerText)
+            })
+        }
     }
-}
